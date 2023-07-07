16a27300b6c6963e258479560f41c2ef06f94e2f
MioszR <99885857+MioszR@users.noreply.github.com>
2023-07-07 20:34:24 +0200

    Add function to play round

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function getComputerChoice() {
     const random = Math.floor(Math.random() * choice.length);
     return choice[random];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+    } else if (playerSelection === "rock" && computerSelection === "paper" || 
+    playerSelection === "paper" && computerSelection === "scissors" ||
+    playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++;
+    } else {
+        playerScore++;
+    }
+}
