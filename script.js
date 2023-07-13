76624b1bed5337c8fef4f102866b498874784b98
MioszR <99885857+MioszR@users.noreply.github.com>
2023-07-13 18:39:20 +0200

    Add buttons and events to them

--- script.js
+++ script.js
@@ -18,19 +18,50 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game(playRound) {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("What is your choice? (Rock, Paper, Scissors): ");
-        playerSelection.toLowerCase();
-        const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection)
-        console.log(computerScore);
-        console.log(playerScore);
-    }
-}
-console.log(game(playRound));
-if (playerScore > computerScore) {
-    console.log("You win everything!")
-} else {
-    console.log("You lost everything!")
-}
+//function game(playRound) {
+//    for (let i = 0; i < 5; i++) {
+//        const playerSelection = prompt("What is your choice? (Rock, Paper, Scissors): ");
+//        playerSelection.toLowerCase();
+//        const computerSelection = getComputerChoice();
+//        playRound(playerSelection, computerSelection)
+//        console.log(computerScore);
+//        console.log(playerScore);
+//    }
+//}
+
+const container = document.querySelector('#container');
+
+const rockButton = document.createElement('button');
+rockButton.classList.add('rockButton');
+rockButton.textContent = 'Rock';
+rockButton.addEventListener('click', () => {
+    playerSelection = "rock";
+    alert(playerSelection)
+})
+const scissorsButton = document.createElement('button');
+scissorsButton.classList.add('scissorsButton');
+scissorsButton.textContent = 'scissors';
+scissorsButton.addEventListener('click', () => {
+    playerSelection = "scissors";
+    alert(playerSelection)
+})
+const paperButton = document.createElement('button');
+paperButton.classList.add('paperButton');
+paperButton.textContent = 'Paper';
+paperButton.addEventListener('click', () => {
+    playerSelection = 'paper'
+    alert(playerSelection)
+})
+
+container.appendChild(rockButton);
+container.appendChild(paperButton);
+container.appendChild(scissorsButton);
+
+
+
+
+
+
+
+
+
